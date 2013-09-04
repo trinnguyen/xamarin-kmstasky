c437681cc4c7f7f8e25a5762706853b90ef2551b
trinnguyen <asdasd@gmail.com>
2013-09-04 14:02:56 +0700

    New android project - done

---
+++ Tasky.Core/BL/Managers/TaskManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tasky.Core.BL.Managers
+{
+    public class TaskManager
+    {
+        static TaskManager()
+        {
+        }
+
+        public static Task GetTask(int id)
+        {
+            return DAL.TaskyManager.GetTask(id);
+        }
+
+        public static IList<Task> GetTasks()
+        {
+            return new List<Task>(DAL.TaskyManager.GetTasks());
+        }
+
+        public static int SaveTask(Task item)
+        {
+            return DAL.TaskyManager.SaveTask(item);
+        }
+
+        public static int DeleteTask(int id)
+        {
+            return DAL.TaskyManager.DeleteTask(id);
+        }
+    }
+}
